454017403bee561642c84c89ac1085e82b505ec7
Seung Hak Lee <grisomm@gmail.com>
2022-08-06 14:21:11 +0900

    .

--- demo.py
+++ demo.py
@@ -87,7 +87,12 @@ def demo(opt):
                     pred_max_prob = pred_max_prob[:pred_EOS]
 
                 # calculate confidence score (= multiply of pred_max_prob)
-                confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+                try:
+                  confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+                except IndexError:
+                  print(f'[error] confidence_score - {img_name}')
+                  continue
+
 
                 print(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}')
                 log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
